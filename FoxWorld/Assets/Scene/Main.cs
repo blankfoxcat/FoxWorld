6d1d4039c76fa902742cb377c4bcb922f8d61ad6
Fox <318642301@qq.com>
2019-04-16 23:58:46 +0800

    添加入口类

---
+++ FoxWorld/Assets/Scene/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 游戏入口
+public class Main : MonoBehaviour
+{
+
+    protected void Start()
+    {
+        Init();
+    }
+
+    protected void Update()
+    {
+        Tick();
+
+    }
+
+    protected void Init()
+    {
+    }
+
+    public void Tick()
+    {
+    }
+}
